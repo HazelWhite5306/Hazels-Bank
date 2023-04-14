488f97d926008cbec260e6b49cff5dec0ab812b2
HazelWhite5306 <haze.white5306@gmail.com>
2023-04-14 10:13:40 -0500

    connecting to database and Adding data practice

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import mysql.connector
+
+connection = mysql.connector.connect(user = "root", database = "example", password = "NaneLimonKabugu16^")
+
+cursor = connection.cursor()
+
+addData = ("INSERT INTO example (Idstudent, Age, Name) VALUES (4,"Jordan",12)")
+
+cursor.execute(addData)
+
+connection.commit()
+
+cursor.close()
+
+connection.close()
